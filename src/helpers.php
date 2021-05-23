e76cfd4b4826c5991a43d9ed15113176956d8410
rgg7888 <rgg7888@outlook.com>
2021-05-22 20:11:19 -0600

    testing

--- src/helpers.php
+++ src/helpers.php
@@ -22,7 +22,7 @@ if(!function_exists('validar_form')) {
     function validar_form(array $names, $buttonName = 'submit', $imprimir = false) {
         $objeto = new App\data\controller\DataController;
         if($objeto->submited($buttonName)) {
-            return $objeto->recorrerResult($objeto->vacio($names),$imprimir);
+            return $objeto->OrdenamientoEspecial($objeto->recorrerResult($objeto->vacio($names),$imprimir));
         }
     }
 }
@@ -31,7 +31,7 @@ if(!function_exists('_validar_form')) {
     function _validar_form(array $names, $buttonName = 'submit', $imprimir = false) {
         $objeto = new App\data\controller\DataController;
         if($objeto->_submited($buttonName)) {
-            return $objeto->recorrerResult($objeto->_vacio($names),$imprimir);
+            return $objeto->OrdenamientoEspecial($objeto->recorrerResult($objeto->_vacio($names),$imprimir));
         }
     }
 }
