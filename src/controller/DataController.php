0d2f869750834283b240bfb7acc182f7604ac653
rgg7888 <rgg7888@outlook.com>
2021-05-22 19:36:47 -0600

    testing

--- src/controller/DataController.php
+++ src/controller/DataController.php
@@ -142,7 +142,7 @@ class DataController {
                     array_push($values,$result[$i]);
                 }
             }
-            return $values;
+            var_dump($values) ;
         }
     }
 
